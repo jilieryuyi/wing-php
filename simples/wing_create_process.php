61ac075f18d543efd58273a19d4e41b632bc799f
yuyi <297341015@qq.com>
2016-05-26 13:59:47 +0800

    u

--- simples/wing_create_process.php
+++ simples/wing_create_process.php
@@ -27,4 +27,27 @@ if($process_id == WING_ERROR_FAILED){
     exit;
 }
 
-echo "exit with ",wing_process_wait($process_id),"\n";
+//WING_INFINITE 永不超时 即线程不退出 一直等待
+$exit_code =  wing_thread_wait($process_id,WING_INFINITE);
+
+if($exit_code == WING_WAIT_TIMEOUT){
+    echo "wait timeout";
+    exit;
+}
+
+if($exit_code == WING_WAIT_FAILED){
+    echo "wait fail";
+    exit;
+}
+
+if($exit_code == WING_WAIT_ABANDONED){
+    echo "mutex not release";
+    exit;
+}
+
+if($exit_code == WING_ERROR_FAILED){
+    echo "get exit code fail";
+    exit;
+}
+
+echo $process_id," is exit with ",$exit_code,"\n";
