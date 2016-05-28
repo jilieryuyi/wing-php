3842eea5424015bc0be64c71cc83f2654fcf68cd
yuyi <297341015@qq.com>
2016-05-28 08:12:50 +0800

    windows crontab demo

--- simples/wing_crontab.php
+++ simples/wing_crontab.php
@@ -7,12 +7,11 @@
  */
 
 //-l 或者 l指令支持
-$show_crontab_list = (isset($argv[1])&&($argv[1]=="-l"||$argv[1]=="l"))?true:false;
+$show_crontab_list = ( isset($argv[1]) && ( $argv[1] == "-l" || $argv[1] == "l") )? true:false;
 if( $show_crontab_list ){
-    $contents = file_get_contents(__DIR__."/wing_contab.conf");
+    $contents       = file_get_contents(__DIR__."/wing_contab.conf");
     $crontabs       = explode("\n",$contents);
     foreach ( $crontabs as $crontab ) {
-
         $_crontab = trim($crontab);
         if (strpos($_crontab, "#") === 0)
             continue;
@@ -74,91 +73,71 @@ function isTimeRule($times_tab){
     debuglog("check:".$times_tab);
     //##秒 分 时 日 月  星期几
     //*/1  *  * *  *   *
-    $tabs = explode(" ",$times_tab);
-    /*for($index = count($tabs)-1;$index >= 0;$index--){
-        if($tabs[$index] == "*") continue;
-        if(strpos($tabs[$index],",") !== false){
+    $tabs       = explode(" ",$times_tab);
+    $times_b    = [date("s"), date("m"), date("H"), date("d"), date("m"), date("w")];
+    $checks     = [];
 
-        }
-    }*/
-    //除了数字还有几个个特殊的符号就是"*"、"/"和"-"、","，
-    //*代表所有的取值范围内的数字，"/"代表每的意思,"*/5"表示每5个单位，
-    //"-"代表从某个数字到某个数字,","分开几个离散的数字
+    foreach( $tabs as $index => $tab )
+    {
 
-
-    $times_b=[
-        date("s"),
-        date("m"),
-        date("H"),
-        date("d"),
-        date("m"),
-        date("w")
-    ];
-
-    $checks = [];
-    for($i=0;$i<count($tabs);$i++) {
-
-        $run_second = false;
-        $times_b[$i] = intval($times_b[$i]);
-        if ($tabs[$i] != "*") {
-            if (strpos($tabs[$i], "/") !== false) {
-                $temp = explode("/", $tabs[$i]);
-                if ($temp[0] == "*") {
-                    //每多少秒执行
-                    //$temp[1]; //获取上次的执行时间
+        $check_run          = false;
+        $times_b[$index]    = intval($times_b[$index]);
+        if ( $tab != "*" ) {
+            if ( strpos($tab, "/") !== false ) {
+                $temp       = explode("/", $tab);
+                if ( $temp[0] == "*" ) {
+                    //每多少单位执行
                     //本来这里需要获取上次执行的时间 然后判断是否已经满足
                     //每多少秒执行一次，此处不用这种方法
-
-                    if ($times_b[$i] % $temp[1] == 0) {
-                        $run_second = true;
-                        debuglog("check:".($times_b[$i] % $temp[1]).":true");
+                    if ($times_b[$index] % $temp[1] == 0) {
+                        $check_run = true;
+                        debuglog("check:".($times_b[$index] % $temp[1]).":true");
                     }else{
-                        debuglog("check:".($times_b[$i] % $temp[1]).":false");
+                        debuglog("check:".($times_b[$index] % $temp[1]).":false");
                     }
                 } else if (strpos($temp[0], "-") !== false) {
                     $s_temp = explode("-", $temp[0]);
                     if (($s_temp[1] - $s_temp[0]) < $temp[1]) {
                         //格式错误
-                        $run_second = false;
+                        $check_run = false;
                     }else {
-                        if ($s_temp[0] <= $times_b[$i] && $times_b[$i] <= $s_temp[1]) {
-                            //每多少秒执行
-                            // $temp[1]; //获取上次的执行时间
-                            if ($times_b[$i] % $temp[1] == 0) {
-                                $run_second = true;
+                        if ($s_temp[0] <= $times_b[$index] && $times_b[$index] <= $s_temp[1]) {
+                            //每多少单位执行
+                            if ($times_b[$index] % $temp[1] == 0) {
+                                $check_run = true;
                             }
                         } else {
-                            $run_second = false;
+                            $check_run = false;
                         }
                     }
                 }
             } else {
                 //ok
-                if (strpos($tabs[$i], ",") !== false) {
-                    debuglog($tabs[$i]." check , ok");
+                if (strpos($tab, ",") !== false) {
+                    debuglog($tab." check , ok");
                     //离散
-                    $s_temp = explode(",", $tabs[$i]);
+                    $s_temp = explode(",", $tab);
                     foreach ($s_temp as $s) {
 
-                        debuglog($times_b[$i] ."==". intval($s).
-                        "=".(($times_b[$i] == intval($s))?"true":"false"));
+                        debuglog($times_b[$index] ."==". intval($s).
+                        "=".(($times_b[$index] == intval($s))?"true":"false"));
 
-                        if ($times_b[$i] == intval($s)) {
-                            debuglog($tabs[$i]." check , ok true");
-                            $run_second = true;
+                        if ($times_b[$index] == intval($s)) {
+                            debuglog($tab." check , ok true");
+                            $check_run = true;
                             break;
                         }
                     }
                 } else {
-                    debuglog($tabs[$i]." check ".$times_b[$i] ."==". intval($tabs[$i]));
-                    $run_second = $times_b[$i] == intval($tabs[$i]);
+                    debuglog($tab." check ".$times_b[$index] ."==". intval($tab));
+                    $check_run = $times_b[$index] == intval($tab);
                 }
             }
         }else{
-            $run_second = true;
+            $check_run = true;
         }
 
-        $checks[] = $run_second;
+        $checks[] = $check_run;
     }
 
     foreach ($checks as $check){
