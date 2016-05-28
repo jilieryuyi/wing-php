e24baf4a4f8212f0f3a69bbdbd441242cab72917
yuyi <297341015@qq.com>
2016-05-28 10:36:43 +0800

    crontab

--- simples/test.php
+++ simples/test.php
@@ -4,4 +4,6 @@
  * @created 2016/5/26 14:40
  * @email 297341015@qq.com
  */
-echo wing_get_env("email");
+date_default_timezone_set("PRC");
+echo date("w");
+//echo strpos("123.php",".php")==(strlen("123.php")-4)?1:0;
--- simples/wing_crontab.php
+++ simples/wing_crontab.php
@@ -5,6 +5,7 @@
  * @email 297341015@qq.com
  * @windows crontab
  */
+date_default_timezone_set("PRC");
 
 //-l 或者 l指令支持
 $show_crontab_list = ( isset($argv[1]) && ( $argv[1] == "-l" || $argv[1] == "l") )? true:false;
@@ -64,88 +65,339 @@ function paramsFormat($crontab_contents){
             $params = trim($params,"\"");
         }
 
+        if(strpos($params,".php")==(strlen($params)-4) && !file_exists($params)){
+            //支持同级相对路径
+            $params = __DIR__."/".$params;
+        }
+
         $crontabs_format[] =[$times_tab,$exe_path,$params];
     }
     return $crontabs_format;
 }
 
-function isTimeRule($times_tab){
-    debuglog("check:".$times_tab);
-    //##秒 分 时 日 月  星期几
-    //*/1  *  * *  *   *
+
+/**
+ * @秒规则校验
+ */
+function checkSecondRule($times_tab){
     $tabs       = explode(" ",$times_tab);
-    $times_b    = [date("s"), date("m"), date("H"), date("d"), date("m"), date("w")];
-    $checks     = [];
+    $tab        = $tabs[0];
+    $times_b    = date("s");
 
-    foreach( $tabs as $index => $tab )
-    {
+    if ( $tab == "*" )
+        return true;
 
-        $check_run          = false;
-        $times_b[$index]    = intval($times_b[$index]);
-        if ( $tab != "*" ) {
-            if ( strpos($tab, "/") !== false ) {
-                $temp       = explode("/", $tab);
-                if ( $temp[0] == "*" ) {
-                    //每多少单位执行
-                    //本来这里需要获取上次执行的时间 然后判断是否已经满足
-                    //每多少秒执行一次，此处不用这种方法
-                    if ($times_b[$index] % $temp[1] == 0) {
-                        $check_run = true;
-                        debuglog("check:".($times_b[$index] % $temp[1]).":true");
-                    }else{
-                        debuglog("check:".($times_b[$index] % $temp[1]).":false");
-                    }
-                } else if (strpos($temp[0], "-") !== false) {
-                    $s_temp = explode("-", $temp[0]);
-                    if (($s_temp[1] - $s_temp[0]) < $temp[1]) {
-                        //格式错误
-                        $check_run = false;
-                    }else {
-                        if ($s_temp[0] <= $times_b[$index] && $times_b[$index] <= $s_temp[1]) {
-                            //每多少单位执行
-                            if ($times_b[$index] % $temp[1] == 0) {
-                                $check_run = true;
-                            }
-                        } else {
-                            $check_run = false;
-                        }
-                    }
-                }
-            } else {
-                //ok
-                if (strpos($tab, ",") !== false) {
-                    debuglog($tab." check , ok");
-                    //离散
-                    $s_temp = explode(",", $tab);
-                    foreach ($s_temp as $s) {
-
-                        debuglog($times_b[$index] ."==". intval($s).
-                        "=".(($times_b[$index] == intval($s))?"true":"false"));
-
-                        if ($times_b[$index] == intval($s)) {
-                            debuglog($tab." check , ok true");
-                            $check_run = true;
-                            break;
-                        }
-                    }
-                } else {
-                    debuglog($tab." check ".$times_b[$index] ."==". intval($tab));
-                    $check_run = $times_b[$index] == intval($tab);
+    if ( strpos($tab, "/") === false ) {
+        if (strpos($tab, ",") !== false) {
+            //离散
+            $s_temp = explode(",", $tab);
+            foreach ($s_temp as $s) {
+                if ($times_b == intval($s)) {
+                    return true;
                 }
             }
-        }else{
-            $check_run = true;
+        } else {
+            return $times_b == intval($tab);
         }
-
-        $checks[] = $check_run;
     }
 
-    foreach ($checks as $check){
-        if(!$check)return false;
+
+    $temp  = explode("/", $tab);
+    if ( $temp[0] == "*" ) {
+        //每多少单位执行
+        //本来这里需要获取上次执行的时间 然后判断是否已经满足
+        //每多少秒执行一次，此处不用这种方法
+        if ($times_b % $temp[1] == 0) {
+            return true;
+        }
     }
 
-    return true;
+    if (strpos($temp[0], "-") === false)
+        return false;
+
+    $s_temp = explode("-", $temp[0]);
+    if (($s_temp[1] - $s_temp[0]) < $temp[1]) {
+        //格式错误
+        return false;
+    }
+
+    if ($s_temp[0] <= $times_b && $times_b <= $s_temp[1]) {
+        //每多少单位执行
+        if ($times_b % $temp[1]  == 0) {
+            return true;
+        }
+    }
+
+    return false;
 }
+
+function checkMinuteRule($times_tab){
+    $tabs       = explode(" ",$times_tab);
+    $tab        = $tabs[1];
+    $times_b    = time();
+
+    if ( $tab == "*" )
+        return true;
+
+    if ( strpos($tab, "/") === false ) {
+        if (strpos($tab, ",") !== false) {
+            //离散
+            $s_temp = explode(",", $tab);
+            foreach ($s_temp as $s) {
+                if ($times_b == strtotime(date("Y-m-d H:$s:0"))) {
+                    return true;
+                }
+            }
+        } else {
+            return $times_b == strtotime(date("Y-m-d H:$tab:0"));
+        }
+    }
+
+
+    $temp  = explode("/", $tab);
+    if ( $temp[0] == "*" ) {
+        //每多少单位执行
+        //本来这里需要获取上次执行的时间 然后判断是否已经满足
+        //每多少秒执行一次，此处不用这种方法
+        if ($times_b % ($temp[1]*60) == 0) {
+            return true;
+        }
+    }
+
+    if (strpos($temp[0], "-") === false)
+        return false;
+
+    $s_temp = explode("-", $temp[0]);
+    if (($s_temp[1] - $s_temp[0]) < $temp[1]) {
+        //格式错误
+        return false;
+    }
+
+    if ( strtotime(date("Y-m-d H:".$s_temp[0].":0")) <= $times_b &&
+         $times_b <= strtotime(date("Y-m-d H:".$s_temp[1].":0"))
+    ) {
+        //每多少单位执行
+        if ($times_b % ($temp[1]*60)  == 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+
+
+function checkHourRule($times_tab){
+    $tabs       = explode(" ",$times_tab);
+    $tab        = $tabs[2];
+    $times_b    = time();
+
+    if ( $tab == "*" )
+        return true;
+
+    if ( strpos($tab, "/") === false ) {
+        if (strpos($tab, ",") !== false) {
+            //离散
+            $s_temp = explode(",", $tab);
+            foreach ($s_temp as $s) {
+                if ($times_b == strtotime(date("Y-m-d $s:0:0"))) {
+                    return true;
+                }
+            }
+        } else {
+            return $times_b == strtotime(date("Y-m-d $tab:0:0"));
+        }
+    }
+
+
+    $temp  = explode("/", $tab);
+    if ( $temp[0] == "*" ) {
+        //每多少单位执行
+        //本来这里需要获取上次执行的时间 然后判断是否已经满足
+        //每多少秒执行一次，此处不用这种方法
+        if ($times_b % ($temp[1]*3600) == 0) {
+            return true;
+        }
+    }
+
+    if (strpos($temp[0], "-") === false)
+        return false;
+
+    $s_temp = explode("-", $temp[0]);
+    if (($s_temp[1] - $s_temp[0]) < $temp[1]) {
+        //格式错误
+        return false;
+    }
+
+    if ( strtotime(date("Y-m-d ".$s_temp[0].":0:0")) <= $times_b &&
+        $times_b <= strtotime(date("Y-m-d ".$s_temp[1].":0:0"))
+    ) {
+        //每多少单位执行
+        if ($times_b % ($temp[1]*3600)  == 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+function checkDayRule($times_tab){
+    //##秒 分 时 日 月  星期几
+    //*  *  * *  *   *
+    $tabs       = explode(" ",$times_tab);
+    $tab        = $tabs[3];
+    $times_b    = time();
+
+    if ( $tab == "*" )
+        return true;
+
+    if ( strpos($tab, "/") === false ) {
+        if (strpos($tab, ",") !== false) {
+            //离散
+            $s_temp = explode(",", $tab);
+            foreach ($s_temp as $s) {
+                if ($times_b == strtotime(date("Y-m-$s 0:0:0"))) {
+                    return true;
+                }
+            }
+        } else {
+            return $times_b == strtotime(date("Y-m-$tab 0:0:0"));
+        }
+    }
+
+
+    $temp  = explode("/", $tab);
+    if ( $temp[0] == "*" ) {
+        //每多少单位执行
+        //本来这里需要获取上次执行的时间 然后判断是否已经满足
+        //每多少秒执行一次，此处不用这种方法
+        if ($times_b % ($temp[1]*86400) == 0) {
+            return true;
+        }
+    }
+
+    if (strpos($temp[0], "-") === false)
+        return false;
+
+    $s_temp = explode("-", $temp[0]);
+    if (($s_temp[1] - $s_temp[0]) < $temp[1]) {
+        //格式错误
+        return false;
+    }
+
+    if ( strtotime(date("Y-m-".$s_temp[0]." 0:0:0")) <= $times_b &&
+        $times_b <= strtotime(date("Y-m-".$s_temp[1]." 0:0:0"))
+    ) {
+        //每多少单位执行
+        if ($times_b % ($temp[1]*86400)  == 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//每月默认按照30天计算
+function checkMonthRule($times_tab){
+    //##秒 分 时 日 月  星期几
+    //*  *  * *  *   *
+    $tabs       = explode(" ",$times_tab);
+    $tab        = $tabs[4];
+    $times_b    = time();
+
+    if ( $tab == "*" )
+        return true;
+
+    if ( strpos($tab, "/") === false ) {
+        if (strpos($tab, ",") !== false) {
+            //离散
+            $s_temp = explode(",", $tab);
+            foreach ($s_temp as $s) {
+                if ($times_b == strtotime(date("Y-$s-1 0:0:0"))) {
+                    return true;
+                }
+            }
+        } else {
+            return $times_b == strtotime(date("Y-$tab-1 0:0:0"));
+        }
+    }
+
+
+    $temp  = explode("/", $tab);
+    if ( $temp[0] == "*" ) {
+        //每多少单位执行
+        //本来这里需要获取上次执行的时间 然后判断是否已经满足
+        //每多少秒执行一次，此处不用这种方法
+        if ($times_b % ($temp[1]*2592000) == 0) {
+            return true;
+        }
+    }
+
+    if (strpos($temp[0], "-") === false)
+        return false;
+
+    $s_temp = explode("-", $temp[0]);
+    if (($s_temp[1] - $s_temp[0]) < $temp[1]) {
+        //格式错误
+        return false;
+    }
+
+    if ( strtotime(date("Y-".$s_temp[0]."-1 0:0:0")) <= $times_b &&
+        $times_b <= strtotime(date("Y-".$s_temp[1]."-1 0:0:0"))
+    ) {
+        //每多少单位执行
+        if ($times_b % ($temp[1]*2592000)  == 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+function getNextWeekTime($week){
+    $i=1;
+    while ($i<14){
+        if(date("w",time()+86400*$i) == $week){
+            return strtotime(date("Y-m-d 00:00:00",time()+86400*$i));
+        }
+        $i++;
+    }
+    return false;
+}
+
+function checkWeekRule($times_tab){
+    //##秒 分 时 日 月  星期几
+    //*  *  * *  *   *
+    $tabs       = explode(" ",$times_tab);
+    $tab        = $tabs[5];
+    $times_b    = time();
+    $week       = date("w");
+
+    if ( $tab == "*" )
+        return true;
+
+    if (strpos($tab, ",") !== false) {
+        //离散
+        $s_temp = explode(",", $tab);
+        foreach ($s_temp as $s) {
+            $_time = getNextWeekTime($s);
+            debuglog("1、".$times_b."=".$_time);
+            if ($week == $s && $times_b == $_time) {
+                return true;
+            }
+        }
+    } else {
+        $_time = getNextWeekTime($tab);
+        debuglog("2、".$times_b."=".$_time);
+        return $week == $tab && $times_b == $_time;
+    }
+
+    return false;
+}
+
 function debuglog($content){
     file_put_contents(__DIR__."/wing_crontab_debug.log",$content."\r\n\r\n",FILE_APPEND);
 }
@@ -154,7 +406,13 @@ function crontab(){
     $crontabs_format    = paramsFormat($crontab_contents);
 
     foreach ($crontabs_format as $crontab){
-        if(isTimeRule($crontab[0])){
+        if( checkSecondRule($crontab[0]) &&
+            checkMinuteRule($crontab[0]) &&
+            checkHourRule($crontab[0]) &&
+            checkDayRule($crontab[0]) &&
+            checkMonthRule($crontab[0]) &&
+            checkWeekRule($crontab[0])
+        ){
             debuglog("run: ".$crontab[1]." ".$crontab[2]);
             wing_create_process($crontab[1],$crontab[2]);
         }
